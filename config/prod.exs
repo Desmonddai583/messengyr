8bbdac35c828dbc54e0e6a51bb28335f19cb4b75
desmonddai583 <desmonddai583@gmail.com>
2017-11-06 15:19:05 +0800

    fixing deployment issue

--- config/prod.exs
+++ config/prod.exs
@@ -1,14 +1,11 @@
 use Mix.Config
 
 config :messengyr, Messengyr.Endpoint,
-  http: [port: {:system, "PORT"}],
-  root: ".",
-  server: true,
-  # Make sure your change the URL to your app's URL:
+  load_from_system_env: true,
   url: [scheme: "https", host: "desmonddai583-messengyr.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :logger, level: :info
 
